48d0ef09f77db6f0d8990ada6157476c7134cdae
Khush Patel <patelxkhush@gmail.com>
2024-09-02 09:58:40 +0530

    Update 1standlast.c

--- C lang/pr-3/1standlast.c
+++ C lang/pr-3/1standlast.c
@@ -11,9 +11,13 @@ i = n % 10;
 while(n>=10){
     n /= 10;
 }
+  if(n<10){
+   printf("The sum of the first and last digit : %d", n);
+  }
 ans = i + n;
 printf("The sum of the first and last digit : %d", ans);
 
 getch();
+  // return 0;
 
  }
