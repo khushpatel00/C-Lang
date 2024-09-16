2c21829b206cd17d3adc8081f16460fa05bd81bd
Khush Patel <patelxkhush@gmail.com>
2024-09-16 19:24:56 +0530

    Add files via upload

--- C lang/pattern loops/2_1.c
+++ C lang/pattern loops/2_1.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <conio.h>
 main(){
-        int i, j;
 
-        // clrscr();
-                for(i=1;i<=5;i++){
-                        for(j=i;j<=5;j++){
-                                if(j==2) printf("B ");
-                                else if(j==4) printf("D ");
-                                else printf("%d ",j);
+// 1 B 3 D 5 
+// B 3 D 5 
+// 3 D 5 
+// D 5 
+// 5
+        int i, j;
+        char ch = 'A';
+
+        clrscr();
+                for(i=1;i<=5;i++)
+                {
+                        for(j=i;j<=5;j++)
+                        {
+                               
+                        
+                                if(j%2==0) 
+                                {
+                                        printf("%c ",ch+(char)j-1);
+                                }
+                                // else if(j==4) printf("D ");
+                               
+                                else  printf("%d ",j);
                         }
                         printf("\n");
                 }
-        // getch();
+        getch(); 
 } 
