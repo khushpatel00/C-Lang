213ca9f5be4458930952ba6d7db5bf6eb61a54a6
Khush Patel <patelxkhush@gmail.com>
2024-10-10 10:21:35 +0530

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+        int i,j;
+
+        clrscr();
+// 5 5 5 5 5 
+// 4 4 4 4 
+// 3 3 3 
+// 2 2 
+// 1
+        for(i=5;i>=1;i--){
+                for(j=i;j>=1;j--){
+                        printf("%d",i);
+
+                }
+                printf("\n");
+        }
+        
+
+        getch();
+        
+}
---
+++ 19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+        int i,j,n=1;
+
+        clrscr();
+
+        
+        
+
+
+        for(i=1;i<=15;i++){
+                for(j=i;j<=i;j++){
+                        printf("%d ",j);
+                        
+                }
+                // n++;
+                printf("\n");
+
+        }
+        
+
+        getch();
+        
+}
