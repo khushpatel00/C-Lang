47fe01b388c4b34e335f2f56be01b8b3ddcff6f4
Khush Patel <patelxkhush@gmail.com>
2024-10-14 11:01:10 +0530

    Add files via upload

---
+++ PR-7/function.c
@@ -0,0 +1,21 @@
+int sum(int n1, int n2)
+{
+    printf("Addition of %d and %d is : %d\n\n", n1, n2, n1 + n2);
+}
+int minus(int n1, int n2)
+{
+    printf("Substraction of %d and %d is : %d\n\n", n1, n2, n1 - n2);
+}
+int mul(int n1, int n2)
+{
+    printf("Multiplication of %d and %d is : %d\n\n", n1, n2, n1 * n2);
+}
+int divide(int n1, int n2)
+{
+    printf("Division of %d and %d is : %d\n\n", n1, n2, n1 / n2);
+}
+int mod(int n1, int n2)
+{
+    printf("Modules of %d and %d is : %d\n\n", n1, n2, n1 % n2);
+}
+// int exit(){}
