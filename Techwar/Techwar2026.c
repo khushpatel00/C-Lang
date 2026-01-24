42f4afe1808dd24901b7ff637b3069c5dddd4229
Khush Patel <patelxkhush@gmail.com>
2026-01-24 09:29:20 +0530

    Added: Techwar2026.c
    This program prints a pattern of asterisks and numbers, followed by the text 'T E C H W A R 2 0 2 6'. It also prints numbers in descending order.

---
+++ Techwar/Techwar2026.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+	int i,j;
+	for(i = 0; i<=8; i++){
+	for(j =0; j<=i;j++){
+		if(j%2 == 0) {
+			
+		
+		printf("* ");
+	} else{
+		
+	
+		printf("%d ", j);
+	}}
+	printf("\n");
+	}
+	
+	printf("T E C H W A R 2 0 2 6 \n");
+	
+	
+	
+	
+	
+	for(i = 11; i>=0; i--){
+		for(j=3;j<=i; j++){
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+	}
